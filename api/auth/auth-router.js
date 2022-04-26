2c1f38e601fa8789a2991b04083b45311dc807ca
Sallie McKee <salliemckee@Morrigan.local>
2022-04-26 12:46:09 -0500

    setting up models and routers

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
+const { checkUsernameExists, validateRoleName } = require("./auth-middleware");
 const { JWT_SECRET } = require("../secrets"); // use this secret!
-
+const bcrypt = require("bcryptjs");
+const User = require("../users/users-model");
 router.post("/register", validateRoleName, (req, res, next) => {
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
@@ -14,8 +15,15 @@ router.post("/register", validateRoleName, (req, res, next) => {
       "role_name": "angel"
     }
    */
+  const { username, password } = req.body;
+  const { role_name } = req;
+  const hash = bcrypt.hashSync(password, 8);
+  User.add({ username, password: hash, role_name })
+    .then((noIdea) => {
+      res.status(201).json(noIdea);
+    })
+    .catch(next);
 });
-
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
   /**
